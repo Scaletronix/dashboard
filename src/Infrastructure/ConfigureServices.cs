6c47575db72d8ed4ee3eb3887ed25056a3866079
maik-hasler <maikhslr@gmail.com>
2023-01-23 12:09:59 +0100

    Added SQLite as persistence

--- src/Infrastructure/ConfigureServices.cs
+++ src/Infrastructure/ConfigureServices.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Application.Common.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure;
 
 public static class ConfigureServices
 {
-    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(configuration.GetConnectionString("Scaletronix"))
+        );
+
+        services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
         return services;
     }
 }
---
+++ src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+
+public sealed class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+}
